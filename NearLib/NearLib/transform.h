0f1d88ab96ab3ffdfe17c5e0b0375306c1983c5c
i-yuuki <i-yuuki@users.noreply.github.com>
2021-08-29 16:28:00 +0900

    NearLib: Transformの関数たちをconst対応

--- NearLib/NearLib/transform.h
+++ NearLib/NearLib/transform.h
@@ -6,10 +6,10 @@ namespace Near{
 
 class Transform{
 public:
-  Math::Vector3 getForward();
-  Math::Vector3 getRight();
-  Math::Vector3 getUp();
-  Math::Matrix createTransform();
+  Math::Vector3 getForward() const;
+  Math::Vector3 getRight() const;
+  Math::Vector3 getUp() const;
+  Math::Matrix createTransform() const;
   Math::Vector3 position = Math::Vector3(0.0f);
   Math::Quaternion rotation = Math::Quaternion::Identity;
   Math::Vector3 scale = Math::Vector3(1.0f);
