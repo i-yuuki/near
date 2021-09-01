a0b009fbc0d09d3df261c4f7117640be2a32ba4f
i-yuuki <i-yuuki@users.noreply.github.com>
2021-09-01 16:14:00 +0900

    ちょっと重いドアの当たり判定を追加

--- Near/door.cpp
+++ Near/door.cpp
@@ -35,6 +35,26 @@ void Door::uninit(){
   model.reset();
 }
 
+void Door::addColliders(std::function<void(const Near::Collision::BoundingBox3D&)> out){
+  if(!active){
+    Near::Math::Vector3 points[] = {
+      Near::Math::Vector3(-64, 128,  0),
+      Near::Math::Vector3( 64, 128,  0),
+      Near::Math::Vector3(-64,   0,  0),
+      Near::Math::Vector3( 64,   0,  0),
+      Near::Math::Vector3(-64, 128, -6),
+      Near::Math::Vector3( 64, 128, -6),
+      Near::Math::Vector3(-64,   0, -6),
+      Near::Math::Vector3( 64,   0, -6),
+    };
+    auto t = transform.createTransform();
+    for(auto& point : points){
+      point = Near::Math::Vector3::Transform(point, t);
+    }
+    out(Near::Collision::BoundingBox3D::FromPoints(points, 8));
+  }
+}
+
 void Door::activate(){
 }
 
--- Near/door.h
+++ Near/door.h
@@ -1,17 +1,19 @@
 #pragma once
 
+#include <NearLib/collidable.h>
 #include <NearLib/fbx-model.h>
 #include <NearLib/game-object.h>
 
 #include "activatable.h"
 
-class Door : public Near::GameObject, public Activatable{
+class Door : public Near::GameObject, public Near::ICollidable, public Activatable{
 public:
   Door();
   virtual void init(Near::Layer* layer) override;
   virtual void update(float deltaTime) override;
   virtual void draw() override;
   virtual void uninit() override;
+  virtual void addColliders(std::function<void(const Near::Collision::BoundingBox3D&)> out) override;
 protected:
   virtual void activate() override;
   virtual void deactivate() override;
