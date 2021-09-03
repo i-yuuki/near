c920b8d257d96a01e841524fcd255eb3092409da
i-yuuki <i-yuuki@users.noreply.github.com>
2021-09-03 20:55:14 +0900

    NearLib: フォント表示作った気がする

--- NearLib/NearLib/font.h
+++ NearLib/NearLib/font.h
@@ -2,7 +2,7 @@
 
 #include <unordered_map>
 
-#include "shader.h"
+#include "buffer.h"
 #include "texture.h"
 
 namespace Near::Font{
@@ -20,16 +20,35 @@ struct FontChar{
   uint8_t channel;
 };
 
+struct LineMetrics{
+  uint32_t width;
+  const std::string_view text;
+};
+
 class Font : public Asset{
 public:
   virtual ~Font();
   virtual void load(const std::string& path) override;
+  void drawText(const std::string_view text, const Math::Vector2& position, const Math::Vector2& origin, float size, const Math::Color& color);
 private:
+  static constexpr unsigned int VERTEX_BUFFER_RECTS = 1024;
   int fontSize;
   int lineHeight;
   int baseline;
+  int textureWidth;
+  int textureHeight;
   std::unordered_map<uint32_t, FontChar> characters;
   std::vector<Texture> textures;
+  StandardVertexBuffer vertexBuffer;
+  IndexBuffer indexBuffer;
+  Vertex3D vertices[VERTEX_BUFFER_RECTS * 4];
+  unsigned int rectIdx;
+  Texture* texture;
+  FontChar* findChar(uint32_t codepoint);
+  void calcLineWidths(const std::string_view text, std::vector<LineMetrics>& out);
+  void addRect(Texture* texture, float x, float y, float w, float h, const Math::Color& color);
+  void addChar(const FontChar* ch, const Math::Vector2& pos, const Math::Color& color);
+  void flush();
 };
 
 }
--- NearLib/src/font.cpp
+++ NearLib/src/font.cpp
@@ -7,6 +7,7 @@
 #include <iomanip>
 #include <sstream>
 
+#include "utf8.h"
 #include "utils.h"
 
 namespace Near::Font{
@@ -43,6 +44,10 @@ void Font::load(const std::string& path){
           lineHeight = parseInt<int>(value);
         }else if(key == u8"base"){
           baseline = parseInt<int>(value);
+        }else if(key == u8"scaleW"){
+          textureWidth = parseInt<int>(value);
+        }else if(key == u8"scaleH"){
+          textureHeight = parseInt<int>(value);
         }else if(key == u8"pages"){
           textures.resize(parseInt<size_t>(value));
         }
@@ -94,6 +99,126 @@ void Font::load(const std::string& path){
       // いつか対応するかも
     }
   }
+
+  for(int i = 0;i < VERTEX_BUFFER_RECTS * 4;i ++){
+    vertices[i] = {Math::Vector3::Zero, Math::Vector3::Forward, Near::Math::Color(), Near::Math::Vector2::Zero};
+  }
+  vertexBuffer.init(true, VERTEX_BUFFER_RECTS * 4, vertices);
+
+  auto indexCount = VERTEX_BUFFER_RECTS * 6;
+  uint32_t* indices = new uint32_t[indexCount];
+  for(int i = 0, ii = 0, vi = 0;i < VERTEX_BUFFER_RECTS;i ++){
+    indices[ii ++] = vi;
+    indices[ii ++] = vi + 1;
+    indices[ii ++] = vi + 2;
+    indices[ii ++] = vi + 2;
+    indices[ii ++] = vi + 1;
+    indices[ii ++] = vi + 3;
+    vi += 4;
+  }
+  indexBuffer.init(false, indexCount, indices);
+  delete[] indices;
+}
+
+void Font::drawText(const std::string_view text, const Math::Vector2& position, const Math::Vector2& origin, float size, const Math::Color& color){
+  rectIdx = 0;
+  texture = nullptr;
+  float scale = size / fontSize;
+  auto* r = renderer();
+
+  // 行数と各行の幅を求める
+  std::vector<LineMetrics> lines;
+  calcLineWidths(text, lines);
+
+  r->pushWorldTransform();
+  r->applyWorldTransform(Math::Matrix::CreateScale(size / fontSize) * Math::Matrix::CreateTranslation(position.x, position.y, 0));
+  Math::Vector2 drawPos(0, lines.size() * lineHeight * -origin.y);
+  for(auto& line : lines){
+    auto it = line.text.data();
+    auto itEnd = it + line.text.size();
+    drawPos.x = line.width * -origin.x;
+    while(it != itEnd){
+      auto codepoint = utf8::next(it, itEnd);
+      if(auto* charInfo = findChar(codepoint)){
+        addChar(charInfo, drawPos, color);
+        drawPos.x += charInfo->xAdvance;
+      }
+    }
+    drawPos.y += lineHeight;
+  }
+  flush();
+  r->popWorldTransform();
+}
+
+FontChar* Font::findChar(uint32_t codepoint){
+  auto info = characters.find(codepoint);
+  return info == characters.end() ? nullptr : &info->second;
+}
+
+void Font::calcLineWidths(const std::string_view text, std::vector<LineMetrics>& out){
+  uint32_t x = 0;
+  uint32_t width = 0;
+  auto it = text.data();
+  auto itLineStart = it;
+  auto itEnd = it + text.size();
+  while(true){
+    auto codepoint = utf8::next(it, itEnd);
+    if(it == itEnd){
+      out.push_back({width, std::string_view(itLineStart, it - itLineStart)});
+      break;
+    }
+    if(codepoint == u8'\n'){
+      out.push_back({width, std::string_view(itLineStart, it - itLineStart)});
+      itLineStart = it;
+      width = 0;
+      x = 0;
+      continue;
+    }
+    if(auto* charInfo = findChar(codepoint)){
+      x += charInfo->xAdvance;
+      width = std::max(x, width);
+    }
+  }
+}
+
+void Font::addChar(const FontChar* ch, const Math::Vector2& pos, const Math::Color& color){
+  auto* texture = &textures[ch->page];
+  if(texture != this->texture || rectIdx >= VERTEX_BUFFER_RECTS){
+    flush();
+    this->texture = texture;
+  }
+  int vertexIdx = rectIdx * 4;
+  float x = pos.x + ch->xOffset;
+  float y = pos.y + ch->yOffset;
+  float uvX = ch->x / static_cast<float>(textureWidth);
+  float uvY = ch->y / static_cast<float>(textureHeight);
+  float uvW = ch->width  / static_cast<float>(textureWidth);
+  float uvH = ch->height / static_cast<float>(textureHeight);
+  vertices[vertexIdx].position = Math::Vector3(x,             y + ch->height, 0);
+  vertices[vertexIdx].uv       = Math::Vector2(uvX,       uvY + uvH);
+  vertices[vertexIdx ++].color = color;
+  vertices[vertexIdx].position = Math::Vector3(x + ch->width, y + ch->height, 0);
+  vertices[vertexIdx].uv       = Math::Vector2(uvX + uvW, uvY + uvH);
+  vertices[vertexIdx ++].color = color;
+  vertices[vertexIdx].position = Math::Vector3(x,             y,              0);
+  vertices[vertexIdx].uv       = Math::Vector2(uvX,       uvY);
+  vertices[vertexIdx ++].color = color;
+  vertices[vertexIdx].position = Math::Vector3(x + ch->width, y,              0);
+  vertices[vertexIdx].uv       = Math::Vector2(uvX + uvW, uvY);
+  vertices[vertexIdx ++].color = color;
+  rectIdx ++;
+}
+
+void Font::flush(){
+  if(!texture){
+    rectIdx = 0;
+    return;
+  }
+  auto* r = renderer();
+  r->setTexture(texture);
+  vertexBuffer.set(vertices);
+  vertexBuffer.draw(0, rectIdx * 6, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST, indexBuffer);
+  rectIdx = 0;
 }
 
 }
