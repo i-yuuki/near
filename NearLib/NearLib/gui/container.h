5b8c7b27d007278e2521b7a3ba9d4e5fb21d6b3c
i-yuuki <i-yuuki@users.noreply.github.com>
2022-01-18 11:50:36 +0900

    NearLib: Containerできたかも

--- NearLib/NearLib/gui/container.h
+++ NearLib/NearLib/gui/container.h
@@ -1,12 +1,21 @@
 #pragma once
 
+#include <optional>
+
 #include "has-child.h"
 
 namespace Near::GUI{
 
 class Container : public HasChildren{
 public:
+  const std::optional<Length>& getWidth() const;
+  const std::optional<Length>& getHeight() const;
+  void setWidth(const std::optional<Length>& width);
+  void setHeight(const std::optional<Length>& height);
   virtual void layout(const BoxConstraints& constraints) override;
+protected:
+  std::optional<Length> width;
+  std::optional<Length> height;
 };
 
 }
--- NearLib/src/gui/container.cpp
+++ NearLib/src/gui/container.cpp
@@ -5,12 +5,60 @@
 
 namespace Near::GUI{
 
+const std::optional<Length>& Container::getWidth() const{
+  return width;
+}
+
+const std::optional<Length>& Container::getHeight() const{
+  return height;
+}
+
+void Container::setWidth(const std::optional<Length>& width){
+  this->width = width;
+}
+
+void Container::setHeight(const std::optional<Length>& height){
+  this->height = height;
+}
+
 void Container::layout(const BoxConstraints& constraints){
-  layoutSize = Near::Math::Vector2(constraints.minWidth, constraints.minHeight);
+  bool hasMaxWidth = std::isfinite(constraints.maxWidth);
+  bool hasMaxHeight = std::isfinite(constraints.maxHeight);
+  // 自分のサイズを決められそうなら決める
+  BoxConstraints childConstraints;
+  if(width.has_value()){
+    switch(width->unit){
+      case Unit::PX:      layoutSize.x = width->value; break;
+      case Unit::PERCENT: layoutSize.x = hasMaxWidth ? constraints.maxWidth * width->value / 100 : 1; break;
+      default: throw std::exception("Unknown width unit! (missing implementation?)");
+    }
+    layoutSize.x = std::clamp(layoutSize.x, constraints.minWidth, constraints.maxWidth);
+    childConstraints.setMaxWidth(layoutSize.x);
+  }
+  if(height.has_value()){
+    switch(height->unit){
+      case Unit::PX:      layoutSize.y = height->value; break;
+      case Unit::PERCENT: layoutSize.y = hasMaxHeight ? constraints.maxHeight * height->value / 100 : 1; break;
+      default: throw std::exception("Unknown height unit! (missing implementation?)");
+    }
+    layoutSize.y = std::clamp(layoutSize.y, constraints.minHeight, constraints.maxHeight);
+    childConstraints.setMaxHeight(layoutSize.y);
+  }
+
+  Near::Math::Vector2 largestChildSize;
   for(auto child : children){
-    child->layout(constraints);
-    layoutSize.x = std::max(layoutSize.x, child->layoutSize.x);
-    layoutSize.y = std::max(layoutSize.y, child->layoutSize.y);
+    child->layout(childConstraints);
+    child->layoutPosition = Near::Math::Vector2::Zero;
+    largestChildSize = Near::Math::Vector2::Max(largestChildSize, child->layoutSize);
+  }
+  
+  // 自動サイズ
+  if(!width.has_value()){
+    // 三項演算子黒魔法！ｗ (よい子はマネしないでね)
+    layoutSize.x = std::clamp(children.empty() ? hasMaxWidth ? constraints.maxWidth : 1 : largestChildSize.x, constraints.minWidth, constraints.maxWidth);
+  }
+  if(!height.has_value()){
+    layoutSize.y = std::clamp(children.empty() ? hasMaxHeight ? constraints.maxHeight : 1 : largestChildSize.y, constraints.minHeight, constraints.maxHeight);
   }
 }
 
